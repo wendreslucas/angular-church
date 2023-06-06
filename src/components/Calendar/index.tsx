50cbce621789a7e5e84e8494eb07045b0562068d
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2023-06-06 18:56:08 -0300

    wls - continuando calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	CalendarActions,
 	CalendarBody,
@@ -9,21 +9,43 @@ import {
 } from './styles';
 import { CaretLeft, CaretRight } from 'phosphor-react';
 import { getWeekDays } from '../../utils/getWeekDays';
+import dayjs from 'dayjs';
 
 export const Calendar: React.FC = () => {
+	const [currentDate, setCurrentDate] = useState(() => {
+		return dayjs().set('date', 1);
+	});
+
 	const shortWeekDays = getWeekDays({ short: true });
+
+	const currentMonth = currentDate.format('MMMM');
+	const currentYear = currentDate.format('YYYY');
+
+	function handlePreviousMonth() {
+		const previousMonthDate = currentDate.subtract(1, 'month');
+
+		setCurrentDate(previousMonthDate);
+	}
+
+	function handleNextMonth() {
+		const nextMonthDate = currentDate.add(1, 'month');
+
+		setCurrentDate(nextMonthDate);
+	}
+
 	return (
 		<CalendarContainer>
 			<CalendarHeader>
 				<CalendarTitle>
-					Dezembro <span>2022</span>
+					{currentMonth} {'  '}
+					<span>{currentYear}</span>
 				</CalendarTitle>
 
 				<CalendarActions>
-					<button>
+					<button onClick={handlePreviousMonth} title="Previous month">
 						<CaretLeft />
 					</button>
-					<button>
+					<button onClick={handleNextMonth} title="Next month">
 						<CaretRight />
 					</button>
 				</CalendarActions>
--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { CalendarStep } from './CalendarStep';
+import { ConfirmStep } from './ConfirmStep';
 
 export default function ScheduleForm() {
 	return (
 		<div>
 			<CalendarStep />
+			{/*<ConfirmStep />*/}
 		</div>
 	);
 }
